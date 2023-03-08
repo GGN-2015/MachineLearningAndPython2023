2524deecd278c2c354ec3d97a0f9338ecfa9cc96
GGN_2015 <premierbob@qq.com>
2023-03-08 15:27:20 +0800

    +Question1

--- Question1/Code.py
+++ Question1/Code.py
@@ -1,15 +1,18 @@
 import sys
+import json
 
-def MyFactorial(x):
+def MyFactorial(x, display_prompt = True):
     typeError = False
     try:
         x = int(x)
     except:
-        print("Please enter an integer!")
+        if(display_prompt):
+            print("Please enter an integer!")
         typeError = True
     if not typeError:
         if x < 0:
-            print("Please enter a non-negative integer!")
+            if(display_prompt):
+                print("Please enter a non-negative integer!")
         else:
             ans = 1
             for i in range(1, x + 1):
@@ -17,5 +20,44 @@ def MyFactorial(x):
             return ans
     return None
 
+def MyAutoTest(filename):
+    # input testdata from file
+    assert type(filename) == str
+    try:
+        with open(filename, "r", encoding="utf8") as f:
+            data = json.load(f)
+    except:
+        print("file not found: <%s>" % filename)
+        data = []
+
+    # run test and validation
+    assert type(data) == list
+    accepted_cases = 0
+    wong_cases = 0
+    for item in data: # test all test cases
+        assert type(item) == dict
+        assert item.get("input") is not None
+        data_in  = item.get("input")
+        data_ans = item.get("answer")
+        data_out = MyFactorial(data_in, display_prompt=False)
+
+        if(data_ans == data_out):
+            accepted_cases += 1
+        else:
+            wong_cases += 1
+            print("for input: ", data_in, " your output is: ", \
+                data_out, "but answer is: ", data_ans)
+    print("%d Tested, %d Accepted." % 
+        (accepted_cases + wong_cases, accepted_cases))
+
 if __name__ == "__main__":
-    print(MyFactorial(5))
+    if len(sys.argv) == 1: # no command line configuration
+        x = input("Please input an integer: ")
+        print(MyFactorial(x))
+
+    elif len(sys.argv) == 2 and sys.argv[1] == "--test":
+        MyAutoTest('TestData.json')
+
+    else:
+        print("Command line configuration unknown.")
+        print(sys.argv[1:])
--- Question1/README.md
+++ Question1/README.md
@@ -1,8 +1,18 @@
 # Question 1: Factoria
 
+This project is written in the Python language. Before launching it,
+please make sure that you have deployed the Python 3 runtime environment \
+on your computer.
+
+## Relavant Files
+- Code.py
+- TestData.json
+
+## Desciprtion
+
 This project has implemented an integrated workflow that combines 
 testing and implementation, and the main function of the program 
-is to calculate the factorial of an integer.
+is to calculate the factorial of an **integer**.
 
 First, we need to ensure that the data input by the user is definitely 
 an integer. If the string entered by the user cannot be interpreted as 
@@ -16,3 +26,17 @@ integer!" because factorial is only defined on non-negative integers.
 The above function is implemented in the function `MyFactorial`. If the 
 input data is invalid, the function returns `None`. Otherwise, the 
 function returns the factorial of the input data.
+
+## Test and Run
+
+If you want to manually test the functionality of this program, please 
+enter the command `python3 Code.py` in the command line to start the 
+program. You just need to enter the corresponding content as prompted 
+by the program to perform the test.
+
+If you want to use the automated testing feature of this program, please 
+use the command line command `python3 Code.py --test`. The automated 
+testing will use the data from the `testdata.json` file.
+
+The code related to testing is implemented in the function `MyAutoTest`. 
+Test cases can be expanded by adding data to `testdata.json`.
