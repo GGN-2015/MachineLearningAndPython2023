f6f9fbe36b175fa4672f468601e618b083e56f82
GGN_2015 <630230141@qq.com>
2023-04-02 20:32:19 +0800

    +Q6

--- Question6/Code.py
+++ Question6/Code.py
@@ -1,9 +1,11 @@
 from sklearn                 import svm
 from sklearn.datasets        import make_classification
+from sklearn.linear_model    import LassoCV
 from sklearn.metrics         import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes     import GaussianNB
 from sklearn.neighbors       import KNeighborsClassifier
+from sklearn.tree            import DecisionTreeClassifier
 
 import numpy as np
 from math import sqrt
@@ -50,6 +52,27 @@ def fRunKNN(xTrain: np.ndarray, yTrain: np.ndarray, xTest: np.ndarray, yTest: np
     tn, fp, fn, tp = confusion_matrix(yTest, yPred).ravel()
     return (tn, fp, fn, tp)
 
+# tn, fp, fn, tp = fRunDTree(xTrain, yTrain, xTest, yTest)
+def fRunDTree(xTrain: np.ndarray, yTrain: np.ndarray, xTest: np.ndarray, yTest: np.ndarray):
+    __fCheckDataAvailable(xTrain, yTrain, xTest, yTest)
+    # run knn with default argv
+    clf = DecisionTreeClassifier()
+    clf.fit(xTrain, yTrain)
+    yPred = clf.predict(xTest)
+    tn, fp, fn, tp = confusion_matrix(yTest, yPred).ravel()
+    return (tn, fp, fn, tp)
+
+# tn, fp, fn, tp = fRunLasso(xTrain, yTrain, xTest, yTest)
+def fRunLasso(xTrain: np.ndarray, yTrain: np.ndarray, xTest: np.ndarray, yTest: np.ndarray):
+    __fCheckDataAvailable(xTrain, yTrain, xTest, yTest)
+    # run knn with default argv
+    clf = LassoCV()
+    clf.fit(xTrain, yTrain)
+    yPred = clf.predict(xTest)
+    yPred = (yPred > 0.5).astype(int)
+    tn, fp, fn, tp = confusion_matrix(yTest, yPred).ravel()
+    return (tn, fp, fn, tp)
+
 # sn, sp, acc, avc, mcc = fCalculateFromConfusionMatrix(tn, fp, fn, tp)
 def fCalculateFromConfusionMatrix(tn: float, fp: float, fn: float, tp: float):
     sn  = tp / (tp + fn)
@@ -59,8 +82,10 @@ def fCalculateFromConfusionMatrix(tn: float, fp: float, fn: float, tp: float):
     mcc = (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
     return sn, sp, acc, avc, mcc
 
-RUN_FUNCTION_LIST = [fRunSvm, fRunNbayes, fRunKNN]
+RUN_FUNCTION_LIST = [fRunSvm, fRunNbayes, fRunKNN, fRunDTree, fRunLasso]
 
+# If the random seed is different, it's difficult to compare 
+# the training accuracy of different models.
 def __fGetRandomStateByRoundId(rid: int):
     assert type(rid) == int
     assert rid >= 0
@@ -98,7 +123,7 @@ def fRunAverage(fRunFunc, xData: np.ndarray, yData: np.ndarray, testRate: float,
     (snArr, spArr, accArr, avcArr, mccArr) = ([], [], [], [], [])
     assert snArr is not spArr # make sure the id is different
     for rid in range(roundCnt):
-        xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size=testRate
+        xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size=testRate,
                                                         random_state=__fGetRandomStateByRoundId(rid))
         # core function call: fRunFunc is a preict model
         tn, fp, fn, tp = fRunFunc(xTrain, yTrain, xTest, yTest)
@@ -123,4 +148,4 @@ def fTestRun(fRunFunc) -> None:
     })
 
 if __name__ == "__main__":
-    fTestRun(fRunKNN)
+    fTestRun(fRunLasso)
